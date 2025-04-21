e5695120934c200b36e6d9dae42a427bda90b9c6
Tea Araki <teaaraki@hawaii.edu>
2025-04-20 19:43:35 -1000

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,43 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.js", 
   output: {
-    filename: "main.js",
+    filename: "main.js", 
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+    
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader",                 
+          "sass-loader",                
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"], 
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/index.html", 
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css", 
     }),
   ],
 };
