11b166c03360cf7fdf1dc7b040071bc5e7a347be
Tea Araki <teaaraki@hawaii.edu>
2025-04-20 19:21:38 -1000

    w13

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', function(err) {});
 
 module.exports = {
   mode: "development",
